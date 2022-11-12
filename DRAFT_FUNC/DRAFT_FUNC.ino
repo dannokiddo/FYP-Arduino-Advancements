87569fed2dd5e40acac6b92ba3999dc7ead6fb20
dannokiddo <frostzo.danial47@gmail.com>
2022-11-12 12:44:41 +0800

    DRAFT_FUNC added RTC

--- DRAFT_FUNC/DRAFT_FUNC.ino
+++ DRAFT_FUNC/DRAFT_FUNC.ino
@@ -1,6 +1,11 @@
+
 // include libraries
-#include <DHT.h>    //temp & humid
+#include <DHT.h>      //temp & humid
 #include <IRremote.h>
+#include <SPI.h>      //wifi **Week6
+#include <WiFiNINA.h> 
+#include <WiFiUdp.h>  //ntp signal
+#include <RTCZero.h>  //real time clock **
 
 // define pins 
 #define temt6000pin A0
@@ -10,31 +15,129 @@
 
 //define identifiers
 DHT dht(dhtpin, DHTTYPE);
+RTCZero rtc;
+
+int status = WL_IDLE_STATUS;      // Wifi status
+char ssid[] = "Nazrin's Family";  // Wifi SSID
+char pass[] = "cheesecake6";      // Wifi password
+
+const int GMT = +8;               // Time zone constant
 
 void setup() {
-    // Begin Serial Monitor
-    Serial.begin(9600);
+  // Begin Serial Monitor
+  Serial.begin(9600);
 
-    dht.begin();
-    IRsender.begin(transpin);
-    
-    // pinMode define
-    pinMode(temt6000pin, INPUT);
+  WiFiConnect();
+  printWiFiStatus();
+
+  rtc.begin();
+  dht.begin();
+  IrSender.begin(transpin);
+  
+  // pinMode define
+  pinMode(temt6000pin, INPUT);
+
+  //NTP Fetch Service
+  unsigned long epoch;                  // Variable to represent epoch
+  int numberOfTries = 0, maxTries = 6;  // Variable for number of tries to NTP service
+
+    do {    // Get epoch
+      epoch = WiFi.getTime();
+      numberOfTries++;
+    }
+
+    while ((epoch == 0) && (numberOfTries < maxTries));
+
+      if (numberOfTries == maxTries) {
+      Serial.print("NTP unreachable!!");
+      while (1);
+      }
+
+      else {
+      Serial.print("Epoch received: ");
+      Serial.println(epoch);
+      rtc.setEpoch(epoch);
+      Serial.println();
+      }
 }
 
 void loop() {
   
+  printTime();
+
   ambient();
 
   dhtsense();
   
   Serial.println("\n**********************************\n");
+  delay(2000);
 
-  if (hic >= 28) {
+  /*if (hic >= 28) {
     IrSender.sendNEC(0x, 32); //turn AC on
     delay(1000);
-    IrSender.sendNEC(0x, 32); //
+    IrSender.sendNEC(0x, 32); //turn fan on
+  }*/
+}
+
+void WiFiConnect() {
+  // Check if the WiFi module works
+  if (WiFi.status() == WL_NO_SHIELD) {
+    // Wait until WiFi ready
+    Serial.println("WiFi adapter not ready");
+    while (true);
+
   }
+
+      // Establish a WiFi connection
+  while ( status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+
+    // Wait 10 seconds for connection:
+    delay(10000);
+
+  }
+}
+
+void printWiFiStatus() {
+
+  // Print the network SSID
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  
+  // Print the IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // Print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void print2digits(int number) {
+
+  if (number < 10) {
+    Serial.print("0");
+  }
+  Serial.print(number);
+}
+
+void printTime()
+{
+  Serial.print("Time : ");
+  print2digits(rtc.getHours() + GMT);
+  Serial.print(":");
+  
+  print2digits(rtc.getMinutes());
+  Serial.print(":");
+  
+  print2digits(rtc.getSeconds());
+  Serial.println("\n");
 }
 
 void ambient() {
@@ -58,8 +161,8 @@ void dhtsense() {
 
   float hic = dht.computeHeatIndex(t, h, false); //false->notFarhrenheit = Celcius
 
-  Serial.println("Humidity   : " + (String)h + " %");
-  Serial.println("Temperature: " + (String)t + " °C");
-  Serial.println("Heat Index : " + (String)hic + " °C \n");
+  Serial.println("Humidity    : " + (String)h + " %");
+  Serial.println("Temperature : " + (String)t + " °C");
+  Serial.println("Heat Index  : " + (String)hic + " °C \n");
   delay(500);
 }
--- DRAFT_FUNC_Blynk/DRAFT_FUNC_Blynk.ino
+++ DRAFT_FUNC_Blynk/DRAFT_FUNC_Blynk.ino
@@ -4,8 +4,12 @@
 #define BLYNK_AUTH_TOKEN "HfjOm1Ku95G8TjHITGlnsLMwEtVreCru"
 
 // include libraries
-#include <DHT.h>    //temp & humid
+#include <DHT.h>      //temp & humid
 #include <IRremote.h>
+#include <SPI.h>      //wifi **Week6
+#include <WiFiNINA.h> 
+#include <WiFiUdp.h>  //ntp signal
+#include <RTCZero.h>  //real time clock **
 
 // define pins 
 #define temt6000pin A0
@@ -15,31 +19,129 @@
 
 //define identifiers
 DHT dht(dhtpin, DHTTYPE);
+RTCZero rtc;
+
+int status = WL_IDLE_STATUS;      // Wifi status
+char ssid[] = "Nazrin's Family";  // Wifi SSID
+char pass[] = "cheesecake6";      // Wifi password
+
+const int GMT = +8;               // Time zone constant
 
 void setup() {
-    // Begin Serial Monitor
-    Serial.begin(9600);
+  // Begin Serial Monitor
+  Serial.begin(9600);
 
-    dht.begin();
-    IRsender.begin(transpin);
-    
-    // pinMode define
-    pinMode(temt6000pin, INPUT);
+  WiFiConnect();
+  printWiFiStatus();
+
+  rtc.begin();
+  dht.begin();
+  IrSender.begin(transpin);
+  
+  // pinMode define
+  pinMode(temt6000pin, INPUT);
+
+  //NTP Fetch Service
+  unsigned long epoch;                  // Variable to represent epoch
+  int numberOfTries = 0, maxTries = 6;  // Variable for number of tries to NTP service
+
+    do {    // Get epoch
+      epoch = WiFi.getTime();
+      numberOfTries++;
+    }
+
+    while ((epoch == 0) && (numberOfTries < maxTries));
+
+      if (numberOfTries == maxTries) {
+      Serial.print("NTP unreachable!!");
+      while (1);
+      }
+
+      else {
+      Serial.print("Epoch received: ");
+      Serial.println(epoch);
+      rtc.setEpoch(epoch);
+      Serial.println();
+      }
 }
 
 void loop() {
   
+  printTime();
+
   ambient();
 
   dhtsense();
   
   Serial.println("\n**********************************\n");
+  delay(2000);
 
   /*if (hic >= 28) {
     IrSender.sendNEC(0x, 32); //turn AC on
     delay(1000);
     IrSender.sendNEC(0x, 32); //turn fan on
   }*/
+}
+
+void WiFiConnect() {
+  // Check if the WiFi module works
+  if (WiFi.status() == WL_NO_SHIELD) {
+    // Wait until WiFi ready
+    Serial.println("WiFi adapter not ready");
+    while (true);
+
+  }
+
+      // Establish a WiFi connection
+  while ( status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+
+    // Wait 10 seconds for connection:
+    delay(10000);
+
+  }
+}
+
+void printWiFiStatus() {
+
+  // Print the network SSID
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  
+  // Print the IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // Print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void print2digits(int number) {
+
+  if (number < 10) {
+    Serial.print("0");
+  }
+  Serial.print(number);
+}
+
+void printTime()
+{
+  Serial.print("Time : ");
+  print2digits(rtc.getHours() + GMT);
+  Serial.print(":");
+  
+  print2digits(rtc.getMinutes());
+  Serial.print(":");
+  
+  print2digits(rtc.getSeconds());
+  Serial.println("\n");
 }
 
 void ambient() {
@@ -63,8 +165,8 @@ void dhtsense() {
 
   float hic = dht.computeHeatIndex(t, h, false); //false->notFarhrenheit = Celcius
 
-  Serial.println("Humidity   : " + (String)h + " %");
-  Serial.println("Temperature: " + (String)t + " °C");
-  Serial.println("Heat Index : " + (String)hic + " °C \n");
+  Serial.println("Humidity    : " + (String)h + " %");
+  Serial.println("Temperature : " + (String)t + " °C");
+  Serial.println("Heat Index  : " + (String)hic + " °C \n");
   delay(500);
 }
